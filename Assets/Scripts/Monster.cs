527c45b4fffd74543d40eb410fc76c364943459c
DrYueh <mameron2255@gmail.com>
2019-02-17 12:22:56 -0700

    General monster_spawner and monster code

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    public enum Colour{
+        RED,BLUE,GREEN
+    }
+    public Colour colour;
+    public int max_health = 10;
+    public float speed = 0.1f;
+    public Vector2 monster_position;
+    
+    private int cur_health;
+    private Vector2 player_position = new Vector2(0,0);
+    
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+        cur_health = max_health;
         
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        
+    }
+
+    void Move(){
         
     }
 }
