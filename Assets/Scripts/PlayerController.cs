0317082553120aa608c1554befbb5499af55d9ef
Unknown <yiji1@ualberta.ca>
2019-02-17 13:42:20 -0700

    Add sprites and turning player left or right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,11 +14,18 @@ public class PlayerController : MonoBehaviour
     private bool direction;
     private int healthPoints;
 
+    private Sprite bardLeft;
+    private Sprite bardRight;
+    private SpriteRenderer bardRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
         healthPoints = 100;
         direction = true;
+        bardRenderer = gameObject.GetComponent<SpriteRenderer>();
+        bardLeft = Resources.Load<Sprite>("Sprites/BardLeft");
+        bardRight = Resources.Load<Sprite>("Sprites/BardRight");
     }
 
     // Update is called once per frame
@@ -30,19 +37,13 @@ public class PlayerController : MonoBehaviour
             UIController.ShowEndGameScreen();
         }
 
-        // Space changes the instrument into the next one
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-
-        }
-
         // Left and right arrow change the direction the player is facing
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             if (direction)
             {
-                // Change sprite or something
-
+                // Change sprite to face left
+                bardRenderer.sprite = bardLeft;
                 Debug.Log("Facing left");
                 direction = false;
             }
@@ -52,18 +53,18 @@ public class PlayerController : MonoBehaviour
         {
             if (!direction)
             {
-                // Change sprite or something 
-
+                // Change sprite to face right 
+                bardRenderer.sprite = bardRight;
                 Debug.Log("Facing right");
                 direction = true;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        // Space changes the instrument into the next one
+        if (Input.GetKeyDown(KeyCode.Space))
         {
 
         }
-
     }
 
     void ChangeInstrument()
