69d2ee2754c68864353c2642890b3c66a37c65ec
Alex Sampley <sampley@ualberta.ca>
2019-02-17 10:34:22 -0700

    Added speed, to once again make music easier to write.

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -13,20 +13,36 @@ public class Music
     
     /// <summary>
     ///   Create music from stings. Music string should be in the following format
+    ///   <Speed>
+    ///   <Note> <Time>
     ///   <Note> <Time>
     ///   ...
-    ///   where Note is one of U,D,L,R, and Time is the time the note should be played.
-    ///   the file should be sorted by Time.
+    ///   where speed is a float factor to multiply each time by,
+    ///   note is one of U,D,L,R,
+    ///   and Time is the time the note should be played.
+    ///   
+    ///   The file should be sorted by Time.
     /// </summary>
     public Music(IEnumerable<string> instrumentNotes) {
         notes = new List<List<Tuple<float, Note>>>();
 
+        // read each instrument
         foreach (string str in instrumentNotes) {
             notes.Add(new List<Tuple<float, Note>>());
 
             StringReader input = new StringReader(str);
-
             string line;
+            
+            // read speed at the top
+            float speed = 1;
+            line = input.ReadLine();
+            if (line == null || !Single.TryParse(line, out speed)) {
+                Debug.LogError(
+                    "Invalid speed in music: " + line + "\n"
+                    + "Speed set to " + speed
+                );
+            }
+           
             while ((line = input.ReadLine()) != null) {
                 string[] split = line.Split(null);
 
@@ -51,7 +67,7 @@ public class Music
                     default: Debug.LogError("Invalid note in music: " + line); continue;
                 }
 
-                notes[notes.Count - 1].Add(new Tuple<float,Note>(time, note));
+                notes[notes.Count - 1].Add(new Tuple<float,Note>(time * speed, note));
             }
         }
     }
