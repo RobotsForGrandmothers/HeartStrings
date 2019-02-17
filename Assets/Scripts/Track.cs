b24f5c94b7de2d5d84eaf011d1930ef86063a5cb
Alex Sampley <sampley@ualberta.ca>
2019-02-16 17:57:06 -0700

    Added some inital music stuff

---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour
+{
+    public TextAsset musicAsset;
+    private Music music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        music = new Music(musicAsset.text);
+        
+        foreach (Tuple<float, Note> m in music.NoteIterator(0)) {
+            Debug.Log("Note: " + m.Item1 + " Time: " + m.Item2);
+        }
+    }
+}
