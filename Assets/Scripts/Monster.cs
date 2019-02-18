0e662a3e69a5c4e43e755010285c28f5f43441a6
DrYueh <mameron2255@gmail.com>
2019-02-17 23:28:10 -0700

    monsters take damage from the waves

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -19,7 +19,6 @@ public class Monster : MonoBehaviour
     // Start is called before the first frame update
     void Start(){
         cur_health = max_health;
-        Debug.Log(this.transform.GetChild(0));
         health_bar = this.transform.GetChild(0);
         //childTransform.localScale += new Vector3(-0.05f,0,0);
     }
@@ -41,6 +40,16 @@ public class Monster : MonoBehaviour
         this.transform.position = Vector2.MoveTowards((Vector2)this.transform.position, player_position, speed);
     }
     
+    Colour colourConverter(int int_colour){
+        if (int_colour == 0){
+            return Colour.RED;
+        }
+        else if (int_colour == 1){
+            return Colour.BLUE;
+        }
+        return Colour.GREEN;
+    }
+    
     void takeDamage(int damage){
         cur_health -= damage;
         float hp_decr_factor = -0.1f * damage / max_health;
@@ -50,10 +59,24 @@ public class Monster : MonoBehaviour
         }
     }
     
+    void stopMovement(){
+        //stop the monster from moving
+    }
+    
     void OnTriggerEnter2D(Collider2D wave){
-        if(wave.gameObject.name == "test_wave"){
+        //Debug.Log("we have a collision");
+        if(wave.gameObject.CompareTag("Wave")){
+            //Debug.Log("we have hit a 'Wave'");
+            GameObject obj = wave.gameObject;//.GetComponent<Wave>();
+            Wave comp = obj.GetComponent<Wave>();
+            int wave_colour = comp.GetColor();
+            if (colourConverter(wave_colour) == this.colour){
+                takeDamage(comp.GetDamage());
+                stopMovement();
+            }
+            //Debug.Log(typeof(comp));
             //int Damage = 2;
-            takeDamage(10);
+            //takeDamage(wave.gameObject.);
             
             //Destroy(this.gameObject);
         }
